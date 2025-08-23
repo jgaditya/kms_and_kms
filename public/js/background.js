74ef85627e12c45e1f99bdb987cacc59374f64c8
snandhanaghub <nandhana20zero4@gmail.com>
2025-08-23 10:12:51 +0530

    Background Shuffle Fix

--- public/js/background.js
+++ public/js/background.js
@@ -2,13 +2,14 @@
 function initBackground() {
     // Array of Kerala background image URLs - using working Unsplash URLs
     const keralaBackgrounds = [
-        'https://images.unsplash.com/photo-1588416499018-d8c621b1b3cf?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80',
-        'https://images.unsplash.com/photo-1585937421610-8da9a6c6d3e7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80',
-        'https://images.unsplash.com/photo-1611655336715-5ce4f5a5b1be?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80',
-        'https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80',
-        'https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80',
-        'https://images.unsplash.com/photo-1598890772482-9b21c2a558c5?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80'
-    ];
+  "https://i.pinimg.com/1200x/25/5b/db/255bdb84cc4f6222c83d8d381c8a8b0f.jpg",
+  "https://i.pinimg.com/1200x/52/67/79/5267799539d251fa6ee16470de28c1fd.jpg",
+  "https://i.pinimg.com/736x/cf/47/32/cf473267e502a271bc214b0ef28c3f6e.jpg",
+  "https://i.pinimg.com/1200x/ad/31/fb/ad31fb203d8c34e555e47fbe76e7af89.jpg",
+  "https://i.pinimg.com/1200x/fc/b0/16/fcb0163f8a3714122a3c5cbdd0735be6.jpg",
+  "https://i.pinimg.com/736x/5f/1c/1e/5f1c1ebe6b59f41f5cb10335e5f4c187.jpg"
+];
+
 
     // Function to set random background
     function setRandomBackground() {
